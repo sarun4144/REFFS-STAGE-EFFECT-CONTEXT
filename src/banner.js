a23a41d5a6215698a9d7e6124d28ee97b523fc5c
sarun4144 <sarun4144@gmail.com>
2022-09-03 01:01:13 +0700

    first commit

---
+++ src/banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function randlnt(max, min) {
+    let x = (min,max) + 1
+  return (
+    min + Math.floor(Math.random() * x)
+  )
+}
+
+export default function Banner() {
+    let banner = ""
+    let url = ""
+
+    switch(r){
+        case 1 : 
+            banner = '../public/banners/'
+    }
+}
