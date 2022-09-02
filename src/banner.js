3c385da86710eeb632a267a2c337580d8984ed4b
sarun4144 <sarun4144@gmail.com>
2022-09-03 01:25:53 +0700

    sarun

--- src/banner.js
+++ src/banner.js
@@ -1,18 +1,41 @@
 import React from 'react'
 
-function randlnt(max, min) {
-    let x = (min,max) + 1
+function randlnt(min, max) {
+    let x = (max-min) + 1
   return (
     min + Math.floor(Math.random() * x)
   )
 }
 
 export default function Banner() {
-    let banner = ""
-    let url = ""
+    const r = randlnt(1,3)
+    let banner = " "
+    let url = " "
 
     switch(r){
         case 1 : 
-            banner = '../public/banners/'
+            banner = './banners/logo-og.png'
+            url = 'https://reactjs.org/'
+            break
+        case 2 : 
+            banner = './banners/React-native.png'
+            url = 'https://www.reactnative.com/'
+            break
+        case 3 : 
+            banner = './banners/nodejs.png'
+            url = 'https://nodejs.org/en/'
+            break
+        default: break ; 
     }
+
+
+    const bannerClick = () => {
+        window.open(url)
+    }
+    return(
+        <div style={{margin:"20px",textAlign:"center"}}>
+            <img src={banner} onClick={bannerClick} style={{cursor:"pointer"}} alt="banner" />
+
+        </div>
+    )
 }
